98baf7b2d4aa34248c70ea1fbeeef090990d57d6
Matheus88 <matheusmessias046@gmail.com>
2021-09-01 00:49:33 -0300

    commit

---
+++ evento.js
@@ -0,0 +1,22 @@
+// Data do evento
+let dataDoEvento = new Date ('2021-09-04')
+let dataDoEvento = new Date ()
+
+if (dataDoEvento > dataDeHoje) {
+    console.log("Evento permitido favor informa a idade")
+}     else{
+    console.log("Evento não permitido")
+
+}
+
+// cadastro de idade 
+const participate = ("Paulo", "Maria", "Pedro")
+let idadePaulo = 19;
+let idadeMaria = 22;
+let idadePedro = 14;
+
+if (idadePedro >= 18){
+    console.log ("Cadatro permitido")
+}   else{
+    console.log ("Cadastro não permitido a menor de 18 anos")
+}
